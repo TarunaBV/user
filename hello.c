ecfca04dabd7d2bfbc27106c3d80be04268fdc35
TarunaBV <tarunabvtarun@gmail.com>
2023-12-03 23:31:35 -0800

    modified 3 files

--- hello.c
+++ hello.c
@@ -1,5 +1,10 @@
 #include<stdio.h>
 int main()
 {
-  printf("Welcome to c programming!")
+  int a;
+  printf("Welcome to c programming!");
+  printf("Enter a number:");
+  scanf("%d",&a);
+  printf("a=%d",a);
+
 }
